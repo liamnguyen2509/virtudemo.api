f993fb1ee904d1934d9c2ac416ef3f05cd0a2813
Liam Nguyen <lam.nguyenthai2509@gmail.com>
2023-12-13 01:15:44 +0700

    allow Cors

--- VirtuDemo.Api/Program.cs
+++ VirtuDemo.Api/Program.cs
@@ -1,11 +1,16 @@
 using Azure.Storage.Blobs;
-using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
 using Microsoft.AspNetCore.Mvc;
 
 # region builder settings
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors();
+builder.Services.Configure<FormOptions>(options =>
+{
+    options.MultipartBodyLengthLimit = long.MaxValue;
+});
 #endregion
 
 #region AppSettings
@@ -25,6 +30,12 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors(builder => builder
+ .AllowAnyOrigin()
+ .AllowAnyMethod()
+ .AllowAnyHeader()
+);
 #endregion
 
 app.MapPost("/api/blobstorage/upload", async ([FromForm] IFormFile formFile, BlobServiceClient blobServiceClient, HttpContext httpContext) =>
@@ -32,7 +43,7 @@ app.MapPost("/api/blobstorage/upload", async ([FromForm] IFormFile formFile, Blo
     if (formFile == null || formFile.Length == 0)
     {
         httpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
-        await httpContext.Response.WriteAsync("Invalid file");
+        await httpContext.Response.WriteAsJsonAsync("Invalid file");
         return;
     }
 
@@ -47,12 +58,12 @@ app.MapPost("/api/blobstorage/upload", async ([FromForm] IFormFile formFile, Blo
         }
 
         httpContext.Response.StatusCode = StatusCodes.Status200OK;
-        await httpContext.Response.WriteAsync("File uploaded successfully to Blob Storage");
+        await httpContext.Response.WriteAsJsonAsync("File uploaded successfully to Blob Storage");
     }
     catch (Exception ex)
     {
         httpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
-        await httpContext.Response.WriteAsync($"Internal server error: {ex.Message}");
+        await httpContext.Response.WriteAsJsonAsync($"Internal server error: {ex.Message}");
     }
 })
 .DisableAntiforgery();
